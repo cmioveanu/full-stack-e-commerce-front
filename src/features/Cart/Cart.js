b3466cd6fe0c065946e05af8f1128a5f255b3738
Cristian <cmioveanu@gmail.com>
2021-04-12 19:25:19 +0100

    update cart styles - add background darkening

--- src/features/Cart/Cart.js
+++ src/features/Cart/Cart.js
@@ -12,38 +12,40 @@ export const Cart = (props) => {
 
 
     return (
-        <section className={styles.cart} id="cart">
-            <div className={styles.productsInCart}>
-                <p>{productsInCart.length} item(s) in your cart</p>
-                <button onClick={props.toggleCart}>X</button>
-            </div>
+        <section className={styles.cart}>
+            <div className={styles.innerContainer}>
+                <div className={styles.productsInCart}>
+                    <p>{productsInCart.length} item(s) in your cart</p>
+                    <button onClick={props.toggleCart}>X</button>
+                </div>
 
-            <div className={styles.productsContainer}>
-                {
-                    productsInCart.map(product => (
-                        <div key={product.id} className={styles.product}>
-                            <div><img src={"/_images/" + product.img_thumb_path}></img></div>
+                <div className={styles.productsContainer}>
+                    {
+                        productsInCart.map(product => (
+                            <div key={product.id} className={styles.product}>
+                                <div><img src={"/_images/" + product.img_thumb_path}></img></div>
 
-                            <div className={styles.productInfo}>
-                                <p className={styles.productName}>{product.name} <br /></p>
-                                <p className={styles.productQuantity}>{product.quantity} x £{product.unit_price}</p>
+                                <div className={styles.productInfo}>
+                                    <p className={styles.productName}>{product.name} <br /></p>
+                                    <p className={styles.productQuantity}>{product.quantity} x £{product.unit_price}</p>
 
-                                <button onClick={() => dispatch(removeFromCart(product))}>-</button>
-                                {product.quantity}
-                                <button onClick={() => dispatch(addToCart(product))}>+</button>
+                                    <button onClick={() => dispatch(removeFromCart(product))}>-</button>
+                                    {product.quantity}
+                                    <button onClick={() => dispatch(addToCart(product))}>+</button>
 
-                                <br />
+                                    <br />
 
-                                <button onClick={() => dispatch(removeAllFromCart(product))}
-                                    className={styles.removeButton}>REMOVE</button>
+                                    <button onClick={() => dispatch(removeAllFromCart(product))}
+                                        className={styles.removeButton}>REMOVE</button>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className={styles.checkoutSection}>
-                <p>FREE SHIPPING ON ALL U.S. ORDERS</p>
-                <p>SUBTOTAL: £{totalCost.toFixed(2)}</p>
+                        ))
+                    }
+                </div>
+                <div className={styles.checkoutSection}>
+                    <p>FREE SHIPPING ON ALL U.S. ORDERS</p>
+                    <p>SUBTOTAL: £{totalCost.toFixed(2)}</p>
+                </div>
             </div>
         </section>
     );

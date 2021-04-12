b15e7868b4b798ce0c55fef267cb8f2a83419ac6
Cristian <cmioveanu@gmail.com>
2021-04-12 16:37:04 +0100

    update header and mobile nav styles

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,17 +1,28 @@
-import React from 'react';
 import styles from './Header.module.css';
-
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
 
-export const Header = (props) => {
+import { logOut } from '../Login/LoginSlice';
 
-    const logMeOut = async () => {
-        await fetch('api/account/logout');
+export const Header = (props) => {
+    const loggedIn = useSelector(state => state.login.loggedIn);
+    const dispatch = useDispatch();
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
+    //log out and redirect to login page
+    const handleLogoutClick = () => {
+        fetch('api/account/logout');
+        dispatch(logOut());
+        setMenuOpen(false);
     }
 
-    const loginLogout = props.name ? <li onClick={logMeOut}><Link to="">Log Out</Link></li>
-        : <li><Link to="/login">Log In</Link></li>;
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    }
 
 
     return (
@@ -19,19 +30,60 @@ export const Header = (props) => {
             <nav className={styles.mainNav}>
                 <div>
                     <h1 className={styles.logo}><Link to="/">The Wooden Shop</Link></h1>
-                    <ul>
-                        <li><Link to="/"></Link></li>
+                    <ul >
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </div>
 
                 <ul>
-                    {loginLogout}
+                    {
+                        !loggedIn ? <li><Link to="/login">Login</Link></li> :
+                            <li><Link to="/login" onClick={handleLogoutClick}>Log out</Link></li>
+                    }
                     <li><Link to="/account">Account</Link></li>
                     <li onClick={props.showHideCart}><span className={styles.cart}>Cart: {props.itemsInCart}</span></li>
                 </ul>
+
+                {/* Hamburger menu icon */}
+                <ul className={styles.cartAndMobileMenu}>
+                    <li onClick={props.showHideCart}><span className={styles.cart}>Cart: {props.itemsInCart}</span></li>
+                    <li>
+                        <svg
+                            viewBox="0 0 100 80"
+                            width="40"
+                            height="40"
+                            data-testid="openMenuButton"
+                            onClick={() => setMenuOpen(true)}
+                        >
+                            <rect width="100" height="10"></rect>
+                            <rect y="30" width="100" height="10"></rect>
+                            <rect y="60" width="100" height="10"></rect>
+                        </svg>
+                    </li>
+                </ul>
             </nav>
+
+            {/* Mobile nav, only displayed for smaller devices */}
+            {   !menuOpen ? null :
+                <nav className={styles.mobileNav}>
+                    <div className={styles.mobileNavInner}>
+                        <button onClick={() => setMenuOpen(false)}>X</button>
+
+                        <div className={styles.linksContainer}>
+                            <ul>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                                {
+                                    !loggedIn ? <li><Link to="/login" onClick={closeMenu}>Login</Link></li> :
+                                        <li><Link to="/login" onClick={handleLogoutClick}>Log out</Link></li>
+                                }
+                                <li><Link to="/account" onClick={closeMenu}>Account</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            }
         </header>
     );
 };
--- src/features/Header/Header.test.js
+++ src/features/Header/Header.test.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from '../../app/store';
+import { logIn } from '../Login/LoginSlice';
 import { Header } from './Header';
 
 
-test('renders Header component', () => {
-    render(<Header />, { wrapper: MemoryRouter });
-
-    const header = screen.getByRole('banner');
-    expect(header).toBeInTheDocument();
+beforeEach(() => {
+    render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <Header />
+            </Provider>
+        </BrowserRouter>
+    );
 });
 
-test('log in link available when logged out', () => {
-    render(<Header name=""/>, { wrapper: MemoryRouter });
 
-    const logInLink = screen.getByText('Log In');
-    expect(logInLink).toBeInTheDocument();
+test('renders logo and correct links', () => {
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+
+    const links = screen.getAllByRole('link');
+
+    expect(links.length).toBe(5);
+    expect(links[0].getAttribute('href')).toBe('/');
+    expect(links[1].getAttribute('href')).toBe('/about');
+    expect(links[2].getAttribute('href')).toBe('/contact');
+    expect(links[3].getAttribute('href')).toBe('/login');
+    expect(links[4].getAttribute('href')).toBe('/account');
 });
 
-test('logout link available when logged in', () => {
-    render(<Header name="Cristian"/>, { wrapper: MemoryRouter });
 
-    const logOutLink = screen.getByText('Log Out');
-    expect(logOutLink).toBeInTheDocument();
+test('log out link renders and works correctly after login', () => {
+    store.dispatch(logIn());
+
+    const links = screen.getAllByRole('link');
+    const logOutLink = links[3];
+    expect(logOutLink.textContent).toBe('Log out');
+
+    fireEvent.click(logOutLink);
+
+    const newLinks = screen.getAllByRole('link');
+    const loginLink = newLinks[3];
+    expect(loginLink.textContent).toBe('Login');
+});
+
+
+test('opens and closes mobile menu', () => {
+    const button = screen.getByTestId('openMenuButton');
+    fireEvent.click(button);
+
+    //two Account links, the regular one, and the one in the modal
+    const accountTexts = screen.getAllByText('Account');
+    expect(accountTexts.length).toBe(2);
+
+    const closeButton = screen.getByText('X');
+    expect(closeButton).toBeInTheDocument();
+    fireEvent.click(closeButton);
+    
+    //modal is closed, so the button is unavailable
+    expect(screen.queryByText('X')).toBeNull();
 });

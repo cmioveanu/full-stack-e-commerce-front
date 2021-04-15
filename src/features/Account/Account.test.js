a6c4c74f500c087663dfdf979a56771aa201ddcd
Cristian <cmioveanu@gmail.com>
2021-04-15 18:30:11 +0100

    update footer test

--- src/features/Account/Account.test.js
+++ src/features/Account/Account.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Account } from './Account';
 
 import { Provider } from 'react-redux';
--- src/features/Footer/Footer.test.js
+++ src/features/Footer/Footer.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
 import { Footer } from './Footer';
 
 
 test('renders Footer component', () => {
     render(<Footer />);
+
     const footer = screen.getByRole('contentinfo');
     expect(footer).toBeInTheDocument();
+
+    const name = screen.getByText('The Wooden');
+    expect(name).toBeInTheDocument();
 });

2aed2279cd7e01e1972290bbdf551f86c4f48608
Cristian <cmioveanu@gmail.com>
2021-04-15 17:37:47 +0100

    add orders component

--- src/features/Account/Account.js
+++ src/features/Account/Account.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './Account.module.css';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -6,9 +5,11 @@ import { useSelector } from 'react-redux';
 import { images } from '../../_images/images';
 import { dateConverter } from '../../utils/helpers';
 
+import { Orders } from './Orders';
+
 
 export const Account = () => {
-    const [orders, setOrders] = useState([]);
+
     const [seeOrders, setSeeOrders] = useState(false);
     const [seeEmail, setSeeEmail] = useState(false);
     const [seePassword, setSeePassword] = useState(false);
@@ -16,17 +17,7 @@ export const Account = () => {
     const loggedIn = useSelector(state => state.login.loggedIn);
 
 
-    //get the list of previous orders
-    useEffect(() => {
-        const ordersList = async () => {
-            const orders = await fetch('/api/orders');
-            const jsonOrders = await orders.json();
 
-            setOrders(jsonOrders);
-        }
-
-        ordersList();
-    }, [])
 
 
     //see orders
@@ -63,38 +54,10 @@ export const Account = () => {
                 <button>Change password</button>
             </section>
 
-
             {  /* Orders Section */
-                !seeOrders ? null :
-                    <section className={styles.ordersHistory}>
-                        {
-                            /* Map the list of orders for display */
-                            orders.map(order => (
-                                <div key={order[0].order_id + order[0].product_id} className={styles.individualOrder}>
-
-                                    <div className={styles.orderDetails}>
-                                        <p className={styles.orderId}># {order[0].order_id} </p>
-                                        <p>{dateConverter((order[0].created_at))}</p>
-                                        <p>Total: £{order[0].totalOrderAmount}</p>
-                                    </div>
-
-                                    {
-                                        /* Map the products inside the order */
-                                        order.map(product => (
-                                            <div key={product.product_id + 'product'} className={styles.productDetails} >
-                                                <img src={images[`img${product.product_id}`]} alt={product.name} />
-                                                <div>
-                                                    <p className={styles.productName}>{product.name}</p>
-                                                    <p>{product.quantity} x £{product.unit_price}</p>
-                                                </div>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            ))
-                        }
-                    </section>
+                !seeOrders ? null : <Orders />
             }
+
         </section>
     );
 }
---
+++ src/features/Account/Orders.js
@@ -0,0 +1,55 @@
+import styles from './Orders.module.css';
+import { useState, useEffect } from 'react';
+
+import { images } from '../../_images/images';
+import { dateConverter } from '../../utils/helpers';
+
+
+export const Orders = () => {
+    const [orders, setOrders] = useState([]);
+
+
+    //get the list of previous orders
+    useEffect(() => {
+        const ordersList = async () => {
+            const orders = await fetch('/api/orders');
+            const jsonOrders = await orders.json();
+
+            setOrders(jsonOrders);
+        }
+
+        ordersList();
+    }, [])
+
+
+    return (
+        <section className={styles.ordersHistory}>
+            {
+                /* Map the list of orders for display */
+                orders.map(order => (
+                    <div key={order[0].order_id + order[0].product_id} className={styles.individualOrder}>
+
+                        <div className={styles.orderDetails}>
+                            <p className={styles.orderId}># {order[0].order_id} </p>
+                            <p>{dateConverter((order[0].created_at))}</p>
+                            <p>Total: £{order[0].totalOrderAmount}</p>
+                        </div>
+
+                        {
+                            /* Map the products inside the order */
+                            order.map(product => (
+                                <div key={product.product_id + 'product'} className={styles.productDetails} >
+                                    <img src={images[`img${product.product_id}`]} alt={product.name} />
+                                    <div>
+                                        <p className={styles.productName}>{product.name}</p>
+                                        <p>{product.quantity} x £{product.unit_price}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+        </section>
+    );
+};

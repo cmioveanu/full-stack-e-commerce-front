b15fb627c9557535a917d6544f82da55ee692d1d
Cristian <cmioveanu@gmail.com>
2021-04-20 18:16:08 +0100

    add stripe checkout

--- src/features/Account/Account.js
+++ src/features/Account/Account.js
@@ -1,5 +1,5 @@
 import styles from './Account.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Orders } from './Orders';
@@ -10,6 +10,7 @@ export const Account = () => {
     const [seeOrders, setSeeOrders] = useState(false);
     const [seeEmail, setSeeEmail] = useState(false);
     const [seePassword, setSeePassword] = useState(false);
+    const [message, setMessage] = useState(null);
 
     const loggedIn = useSelector(state => state.login.loggedIn);
 
@@ -38,6 +39,20 @@ export const Account = () => {
     }
 
 
+    //check for params after redirecting from Stripe checkout
+    useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+
+        if (query.get('success')) {
+            setMessage('Order placed! You will receive an email confirmation.');
+        }
+
+        if (query.get('canceled')) {
+            setMessage('Order canceled! -- continue to shop around and checkout when you\'re ready.');
+        };
+    }, []);
+
+
     return (
         <section className={styles.account}>
             <section className={styles.overview}>
@@ -45,6 +60,8 @@ export const Account = () => {
                     {loggedIn ? 'Welcome back!' : 'Please log in first!'}
                 </h2>
                 <p className={styles.editPara}>You can edit your account details or check out your order history below.</p>
+
+                {!message ? null : <p className={styles.message}>{message}</p>}
 
                 <button onClick={openOrders}>Order history</button>
                 <button onClick={openChangeEmail}>Change email</button>
--- src/features/Cart/Cart.js
+++ src/features/Cart/Cart.js
@@ -6,6 +6,9 @@ import { useEffect } from 'react';
 
 import { images } from '../../_images/images';
 
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_51IiLWaFmwWJ3Dyfc6Tg7PKSoEcaNMTO62LGjrNBCh3YJ34LGDqdaytDx8gCOwe3Czk8HdCMy7MKtCWsl5k54cnwc00CtddBNIs');
+
 
 export const Cart = (props) => {
     const dispatch = useDispatch();
@@ -30,52 +33,79 @@ export const Cart = (props) => {
     });
 
 
+    //Stripe checkout
+    const handleStripeCheckout = async () => {
+        const stripe = await stripePromise;
+
+        try {
+            const response = await fetch('/api/checkout/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const jsonResponse = await response.json();
+
+            await stripe.redirectToCheckout({
+                sessionId: jsonResponse.id
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+
     return (
         <section className={styles.cart}>
-                <div className={styles.innerContainer}>
+            <div className={styles.innerContainer}>
 
-                    {/* items in cart count at the top*/}
-                    <div className={styles.productsInCart}>
-                        <p>{numberOfItems} item(s) in your cart</p>
-                        <button onClick={props.toggleCart}>X</button>
-                    </div>
-
-                    {/* products in cart */}
-                    <div className={styles.productsContainer}>
-                        {
-                            productsInCart.map(product => (
-                                <div key={product.id} className={styles.product}>
-                                    <div><img src={images[`white${product.id}`]} alt={product.name}></img></div>
-
-                                    <div className={styles.productInfo}>
-                                        <p className={styles.productName}>{product.name} <br /></p>
-                                        <p className={styles.productQuantity}>{product.quantity} x £{product.unit_price}</p>
-
-                                        <button
-                                            className={styles.decrement}
-                                            onClick={() => dispatch(removeFromCart(product))}
-                                        >-</button>
-                                        {product.quantity}
-                                        <button
-                                            className={styles.increment}
-                                            onClick={() => dispatch(addToCart(product))}
-                                        >+</button>
-
-                                        <button onClick={() => dispatch(removeAllFromCart(product))}
-                                            className={styles.removeButton}>REMOVE</button>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                    {/* subtotal and checkout */}
-                    <div className={styles.checkoutSection}>
-                        <p>FREE SHIPPING ON ALL U.S. ORDERS</p>
-                        <p>SUBTOTAL: £{totalCost.toFixed(2)}</p>
-                        <button className={styles.checkoutButton}>Go to checkout</button>
-                    </div>
+                {/* items in cart count at the top*/}
+                <div className={styles.productsInCart}>
+                    <p>{numberOfItems} item(s) in your cart</p>
+                    <button onClick={props.toggleCart}>X</button>
                 </div>
+
+                {/* products in cart */}
+                <div className={styles.productsContainer}>
+                    {
+                        productsInCart.map(product => (
+                            <div key={product.id} className={styles.product}>
+                                <div><img src={images[`white${product.id}`]} alt={product.name}></img></div>
+
+                                <div className={styles.productInfo}>
+                                    <p className={styles.productName}>{product.name} <br /></p>
+                                    <p className={styles.productQuantity}>{product.quantity} x £{product.unit_price}</p>
+
+                                    <button
+                                        className={styles.decrement}
+                                        onClick={() => dispatch(removeFromCart(product))}
+                                    >-</button>
+                                    {product.quantity}
+                                    <button
+                                        className={styles.increment}
+                                        onClick={() => dispatch(addToCart(product))}
+                                    >+</button>
+
+                                    <button onClick={() => dispatch(removeAllFromCart(product))}
+                                        className={styles.removeButton}>REMOVE</button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+
+                {/* subtotal and checkout */}
+                <div className={styles.checkoutSection}>
+                    <p>FREE SHIPPING ON ALL U.S. ORDERS</p>
+                    <p>SUBTOTAL: £{totalCost.toFixed(2)}</p>
+                    <button
+                        className={styles.checkoutButton}
+                        onClick={handleStripeCheckout}
+                    >Checkout</button>
+                </div>
+            </div>
         </section>
     );
 }
